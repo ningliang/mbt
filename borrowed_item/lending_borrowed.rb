a73c25224c49d9722eaa812109f232c7006a7636
Ning Liang <nliang@book.com>
2009-09-23 18:47:58 -0400

    adjustments

--- borrowed_item/lending_borrowed.rb
+++ borrowed_item/lending_borrowed.rb
@@ -1,5 +1,7 @@
 require File.dirname(__FILE__) + "/../graph_path_explorer"
 
+$current_user = :none
+
 # Set up
 def generate_setup
   puts "public void setUp() {"
@@ -55,70 +57,66 @@ end
 def code_to_transition(edge, expected)
   code = case edge["name"]    
     when "Offer" then 
-      switch_user("lender") +
-      "\t#{log("Offering")}\n" + 
+      switch_user(:lender) + 
       "\tlendProduct(product, lendee.getEmail(), #{expected});\n" 
     when "Return" then 
-      switch_user("lendee") +
-      "\t#{log("Returning")}\n" + 
+      switch_user(:lendee) +
       "\treturnLend(product, #{expected});"
     when /Accept/ then 
-      switch_user("lendee") +
-      "\t#{log("Accepting")}\n" + 
+      switch_user(:lendee) +
       "\tdecideLend(product, true, #{expected});"
     when /Reject/ then 
-      switch_user("lendee") +
-      "\t#{log("Rejecting")}\n" + 
+      switch_user(:lendee) +
       "\tdecideLend(product, false, #{expected});"
     when "View" then 
-      switch_user("lendee") +
-      "\t#{log("Viewing")}\n" + 
+      switch_user(:lendee) +
       "\tviewLend(product, #{expected});"
     when /Purchase/ then 
-      switch_user("lendee") +
-      "\t#{log("Purchasing")}\n" + 
+      switch_user(:lendee) +
       "\tpurchaseProduct(product, #{expected});"
     when "Start" then ""
     else 
       puts "No expiration yet! #{edge["name"]}"
       Process.exit(-1)
   end
+  puts "\t#{log($current_user.to_s + " " + edge["name"])}"
   puts code
+  puts "\tLockerService.synchronize();"
 end
 
 def code_to_verify(state)
   code = case state
     when "None" then
-      switch_user("lendee") +
+      switch_user(:lendee) +
       "\t#{log("Should be ABSENT")}\n" + 
       "\tassertTrue(LockerService.getContent(product.getEan()) == null);\n"
     when "PendingBorrowed" then
-      switch_user("lendee") +
+      switch_user(:lendee) +
       "\t#{log("Should be PENDING_BORROW")}\n" +
       "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
       "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.PENDING_BORROW));\n"
     when "Accepted" then
-      switch_user("lendee") +
+      switch_user(:lendee) +
       "\t#{log("Should be BORROWED")}\n" +
       "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
       "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.BORROWED));\n"
     when "PendingViewed" then
-      switch_user("lendee") +
+      switch_user(:lendee) +
       "\t#{log("Should be PENDING_VIEWED")}\n" +
       "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
       "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.PENDING_VIEWED));\n"
     when "Expired" then
-      switch_user("lendee") +
+      switch_user(:lendee) +
       "\t#{log("Should be EXPIRED")}\n" +
       "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
       "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.EXPIRED));\n"
     when "Returned" then
-      switch_user("lendee") +
+      switch_user(:lendee) +
       "\t#{log("Should be RETURNED")}\n" +
       "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
       "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.RETURNED));\n"
     else # Deleted
-      switch_user("lendee") +
+      switch_user(:lendee) +
       "\t#{log("Should be ABSENT")}\n" +
       "\tassertTrue(LockerService.getContent(product.getEan()) == null);\n"
   end
@@ -126,11 +124,16 @@ def code_to_verify(state)
 end
 
 def switch_user(user) 
-  "\t#{log("Switching to #{user}")}\n" + 
-  "\tLockerService.resetLocker();\n" +
-  "\tProxyFactory.getDefaultProxy().clearCookies();\n" +
-  "\tDeviceService.register(#{user}.getEmail(), #{user}.getPassword(), #{user}Device, DEVICE_NAME, null, null);\n" +
-  "\tLockerService.synchronize();\n"
+  retval = ""
+#  unless $current_user.eql? user
+#    $current_user = user
+    retval = "\t#{log("Switching to #{user.to_s}")}\n" + 
+    "\tLockerService.resetLocker();\n" +
+    "\tProxyFactory.getDefaultProxy().clearCookies();\n" +
+    "\tDeviceService.register(#{user.to_s}.getEmail(), #{user.to_s}.getPassword(), #{user.to_s}Device, DEVICE_NAME, null, null);\n" +
+    "\tLockerService.synchronize();\n"
+#  end
+  retval
 end
 
 def log(message)
@@ -144,6 +147,7 @@ graph.valid_paths.each do |path|
     generate_test path
   end
 end
+puts "#{graph.valid_paths.length} valid paths."
 
 #graph.error_paths.each do |path|
 #  unless path.find do |edge| /Expire/.match edge["name"] end
