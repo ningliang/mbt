8cfb60e4386ab3a05a68a1867d9214246b269888
Ning Liang <nliang@book.com>
2009-10-28 19:45:47 -0400

    updated for multi graphs

--- borrowed_item/lending_borrowed.rb
+++ borrowed_item/lending_borrowed.rb
@@ -1,176 +1,21 @@
-require File.dirname(__FILE__) + "/../graph_path_explorer"
+require File.dirname(__FILE__) + "/../graph_path_explorer.rb"
 
-$current_user = :none
+explorer = GraphPathExplorer.new("lending_borrowed.json")
+paths = explorer.valid_paths
 
-# Set up
-def generate_setup
-puts <<-eos
-public void setUp() {
-	try {
-		ProxyFactory.getDefaultProxy().clearCookies();
-		LockerService.resetLocker();
-	
-		// Clear cookies
-		lender = randomAccount();
-		lendee = randomAccount();
-		createAccount(lender, null);
-		createAccount(lendee, null);
-		lenderDevice = randomDeviceId();
-		lendeeDevice = randomDeviceId();
-	
-		// Create lendee CC
-		DeviceService.register(lendee.getEmail(), lendee.getPassword(), lendeeDevice, DEVICE_NAME, null, null);
-		createCreditCard(randomCreditCard(), null);
-		updateCreditCardBillingAddress(randomBillingAddress(), null);
-		ProxyFactory.getDefaultProxy().clearCookies();
-		LockerService.resetLocker();
-	
-		// Create lender CC and purchase product
-		DeviceService.register(lender.getEmail(), lender.getPassword(), lenderDevice, DEVICE_NAME, null, null);
-		createCreditCard(randomCreditCard(), null);
-		updateCreditCardBillingAddress(randomBillingAddress(), null);
-		StoreService.purchaseProduct(StoreService.getProduct("9781602150591"));
-		Thread.sleep(5000); // TODO Why does this take so long?!
-		LockerService.synchronize();
-		product = LockerService.getContent("9781602150591");
-		assertTrue(product != null);
-		assertTrue(LockerService.getProductState(product) != null);
-		LockerService.resetLocker();
-		ProxyFactory.getDefaultProxy().clearCookies();
-	} catch (Exception e) {
-		e.printStackTrace(); 
-		assertTrue(false);
-	}
-}
-eos
+puts "VALID"
+
+paths.each do |path|
+  names = path.collect do |transition| transition["name"] end
+  puts names.to_json
 end
 
-# Each path
-def generate_test(path) 
-  title = path.collect do |edge| edge["name"] end.join("")
-  puts "public void test#{title}() throws Exception {"
-  puts "\tSystem.out.println(\"Testing #{title}\");"
-  path.each_index do |i|
-    if i > 0
-      previous = path[i-1]
-      current = path[i]
-      
-      legal = previous["to"].eql? current["from"]
-      if legal 
-        code_to_transition(current, "null")
-        code_to_verify(current["to"]) 
-      else
-        code_to_transition(current, "WebserviceException.class")
-        code_to_verify(previous["to"])
-      end
-    end
-  end
-  puts "}"
-  puts
+puts "INVALID"
+
+invalid_paths = explorer.invalid_paths
+invalid_paths.each do |path|
+  names = path.collect do |transition| transition["name"] end
+  puts names.to_json
 end
 
-#TODO switching lender versus lendee
-def code_to_transition(edge, expected)
-  code = case edge["name"]    
-    when "Offer" then 
-      switch_user(:lender) + 
-      "\tlendProduct(product, lendee.getEmail(), #{expected});\n" 
-    when "Return" then 
-      switch_user(:lendee) +
-      "\treturnLend(product, #{expected});"
-    when /Accept/ then 
-      switch_user(:lendee) +
-      "\tdecideLend(product, true, #{expected});"
-    when /Reject/ then 
-      switch_user(:lendee) +
-      "\tdecideLend(product, false, #{expected});"
-    when "View" then 
-      switch_user(:lendee) +
-      "\tviewLend(product, #{expected});"
-    when /Purchase/ then 
-      switch_user(:lendee) +
-      "\tpurchaseProduct(product, #{expected});"
-    when "Start" then ""
-    else 
-      puts "No expiration yet! #{edge["name"]}"
-      Process.exit(-1)
-  end
-  puts "\t#{log($current_user.to_s + " " + edge["name"])}"
-  puts code
-  puts "\tLockerService.synchronize();"
-end
-
-def code_to_verify(state)
-  code = case state
-    when "None" then
-      switch_user(:lendee) +
-      "\t#{log("Should be ABSENT")}\n" + 
-      "\tassertTrue(LockerService.getContent(product.getEan()) == null);\n"
-    when "PendingBorrowed" then
-      switch_user(:lendee) +
-      "\t#{log("Should be PENDING_BORROW")}\n" +
-      "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
-      "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.PENDING_BORROW));\n"
-    when "Accepted" then
-      switch_user(:lendee) +
-      "\t#{log("Should be BORROWED")}\n" +
-      "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
-      "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.BORROWED));\n"
-    when "PendingViewed" then
-      switch_user(:lendee) +
-      "\t#{log("Should be PENDING_VIEWED")}\n" +
-      "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
-      "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.PENDING_VIEWED));\n"
-    when "Expired" then
-      switch_user(:lendee) +
-      "\t#{log("Should be EXPIRED")}\n" +
-      "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
-      "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.EXPIRED));\n"
-    when "Returned" then
-      switch_user(:lendee) +
-      "\t#{log("Should be RETURNED")}\n" +
-      "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
-      "\tassertTrue(LockerService.getProductState(product).getLendingState().equals(LendingState.RETURNED));\n"
-    when "Purchased" then
-      switch_user(:lendee) +
-      "\tSystem.out.println(\"Current state is \" + LockerService.getProductState(product).getLendingState());\n" +
-      "\tassertTrue(LockerService.getContent(product.getEan()) != null);\n"
-    else # Deleted
-      switch_user(:lendee) +
-      "\t#{log("Should be ABSENT")}\n" +
-      "\tassertTrue(LockerService.getContent(product.getEan()) == null);\n"
-  end
-  puts code
-end
-
-def switch_user(user) 
-  retval = ""
-#  unless $current_user.eql? user
-#    $current_user = user
-    retval = "\t#{log("Switching to #{user.to_s}")}\n" + 
-    "\tLockerService.resetLocker();\n" +
-    "\tProxyFactory.getDefaultProxy().clearCookies();\n" +
-    "\tDeviceService.register(#{user.to_s}.getEmail(), #{user.to_s}.getPassword(), #{user.to_s}Device, DEVICE_NAME, null, null);\n" +
-    "\tLockerService.synchronize();\n"
-#  end
-  retval
-end
-
-def log(message)
-  "System.out.println(\"#{message}\");"  
-end
-
-graph = GraphPathExplorer.new("lending_borrowed.json");
-generate_setup
-graph.valid_paths.each do |path| 
-  unless path.find do |edge| /Expire/.match edge["name"] end
-    generate_test path
-  end
-end
-puts "#{graph.valid_paths.length} valid paths."
-
-#graph.error_paths.each do |path|
-#  unless path.find do |edge| /Expire/.match edge["name"] end
-#    generate_test path
-#  end
-#end
+puts "#{paths.length} valid paths and #{invalid_paths.length} invalid paths"
--- graph_path_explorer.rb
+++ graph_path_explorer.rb
@@ -1,120 +1,143 @@
 require 'rubygems'
 require 'json/ext'
 
-class GraphPathExplorer    
-  # Read in a specification from a file
-  def initialize(file)
-    @all_edge_names = []
-    @edges = {}
-    @edge_name_graph = {}
-    @final_edge_names = []
-    @paths = nil
-    @error_paths = nil
+# TODO handle cycles
+class GraphPathExplorer
+  START = "Start"
+  START_NODE = "Start"
+  
+  def initialize(file) 
+    # Start, ends, results
+    @start = nil
+    @ends = nil
+    @valid_paths = nil
+    @invalid_paths = nil
+        
+    # Graph table (node -> [{name: edge_name, state: destination}])
+    @transitions = {}
+    @invalid_transitions = {}
     
     # Get the JSON from the file and parse it
     data = ""
     File.open(file, "r") do |infile|
       while (line = infile.gets) 
-        data += line
+        data += line 
       end
     end
     specification = JSON.parse(data)
     
+    @ends = specification["end"]
     @start = specification["start"]
     
-    # Fill the graph description, with an additional "Start" state
-    start = { "name" => "Start", "from" => nil, "to" => @start }
+    # Fill the graph description
+    start = { "name" => START, "transitions" => [{"from" => START_NODE, "to" => @start }] }
     specification["edges"].push(start)
     specification["edges"].each do |current|
-      @edges[current["name"]] = current
-      @all_edge_names.push current["name"]
-      @final_edge_names.push current["name"] if specification["end"].include? current["to"]
-      @edge_name_graph[current["name"]] = []
-      specification["edges"].each do |edge|
-        @edge_name_graph[current["name"]].push edge["name"] if current["to"].eql? edge["from"]
+      name = current["name"]
+      current["transitions"].each do |transition|
+        from = transition["from"]
+        to = transition["to"]
+        @transitions[from] = [] unless @transitions[from]
+        @transitions[from].push({ "name" => name, "to" => to })
+      end
+    end
+    
+    # Fill invalid transitions
+    @transitions.keys.each do |node|
+      @invalid_transitions[node] = []
+      specification["edges"].each do |current|
+        legal = false
+        name = current["name"]
+        current["transitions"].each do |transition|
+          legal = true if transition["from"].eql? node
+        end
+        @invalid_transitions[node].push(edge(name, node, node))
       end
     end
   end
   
-  # Return valid paths, generating if necessary
   def valid_paths
-    unless @paths
-      @paths = []
-      stack = []
-      visited_paths = {}
-      stack.push "Start"
+    unless @valid_paths
+      @valid_paths = []   # Saved paths
+      stack = []    # Exploration stack
+      node = nil    # Current node
+      visited = {}
+      
+      # Take the first path
+      previous = nil
+      node = @start
+      stack.push( edge(START, nil, node) )
+      visited[stack.to_json] = true
+      
       until stack.empty?
-        edge_name = stack.last
-        if @final_edge_names.include? edge_name and @paths.length > 0
-          stack.pop
-        else
-          has_new_child_path = false
-          @edge_name_graph[edge_name].each do |next_name|
-            unless stack.include? next_name
-              stack.push next_name
-              hash = stack.join ","
-              if visited_paths[hash]
-                stack.pop
-              else
-                @paths.push snapshot(stack)
-                visited_paths[hash] = true
-                has_new_child_path = true
-                break
+        new_path = false
+        if @transitions.has_key?(node)
+          for transition in @transitions[node]
+            # Save info
+            name = transition["name"]
+            to = transition["to"]
+            from = node
+            
+            stack.push(edge(name, from, to))
+            if visited.has_key?(stack.to_json)
+              stack.pop
+            else
+              visited[stack.to_json] = true
+              new_path = true
+              node = to 
+              @valid_paths.push(snapshot(stack))
+              break
+            end
+          end
+        end
+        
+        unless new_path
+          old_edge = stack.pop
+          node = old_edge["from"]
+        end
+      end
+      
+      @valid_paths = @valid_paths.find_all do |path|
+        transition = path.last
+        @ends.include? transition["to"]
+      end      
+    end
+    @valid_paths
+  end
+  
+  def invalid_paths
+    unless @invalid_paths
+      @invalid_paths = []
+      visited = {}
+      
+      # For each subpath starting at beginning
+      valid_paths.each do |path|
+        for length in 1..(path.length)
+          subarray = path[0, length]
+          state = subarray.last["to"]
+          
+          # For each illegal transition from the last state
+          if @invalid_transitions.has_key? state
+            for transition in @invalid_transitions[state]
+              new_subpath = snapshot(subarray)
+              new_subpath.push(transition)
+              
+              # If new, save it and mark it visited
+              unless visited.has_key? new_subpath.to_json
+                visited[new_subpath.to_json] = true
+                @invalid_paths.push(new_subpath)
               end
             end
-          end
-          stack.pop unless has_new_child_path
+          end          
         end
       end
-      @paths = @paths.find_all do |path| @final_edge_names.include? path.last end
-      @paths = @paths.collect do |path| 
-        path.collect do |edge_name| @edges[edge_name] end
+      
+      @invalid_paths = @invalid_paths.find_all do |path|
+        path.last["name"] != START
       end
     end
-    @paths
+    @invalid_paths
   end
-  
-  # Return error paths, generating paths and error paths if necessary
-  def error_paths
-    valid_paths unless @paths
-    unless @error_paths
-      @error_paths = []
-      
-      # Begin with transitions that don't begin at start node
-      @all_edge_names.each do |edge_name|
-        unless @edges[edge_name]["from"].eql? @start or edge_name.eql? "Start"
-          @error_paths.push [edge_name]
-        end
-      end
-      
-      # Explore all possible error paths off subpaths of valid paths
-      stack = []
-      visited_paths = {}
-      
-      path_names = []
-      @paths.each do |path|
-        (1..(path.length)).each do |n|
-          subpath = path.first(n).collect do |edge| edge["name"] end
-          action = subpath.last       
-          illegal_actions = @all_edge_names.find_all do |item| !@edge_name_graph[action].include? item end
-          illegal_actions.each do |illegal_action|
-            error_path = subpath + [illegal_action]
-            key = error_path.join(",")
-            unless visited_paths[key]
-              visited_paths[key] = true
-              @error_paths.push snapshot(error_path)
-            end
-          end
-        end
-      end
-      @error_paths = @error_paths.collect do |path| 
-        path.collect do |name| @edges[name] end
-      end
-    end
-    @error_paths
-  end  
-  
-  private
   
   # Deep array copy
   def snapshot(path) 
@@ -122,9 +145,12 @@ class GraphPathExplorer
     path.each do |item| ret.push item end
     ret
   end
+  
+  # Construct an edge
+  def edge(name, from, to) 
+    {"name" => name, "from" => from, "to" => to}
+  end
 end
-
-
 
 
 
