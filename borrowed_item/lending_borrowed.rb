4a02bf29f6dcd70082cea89fa4269c44cd2585f1
Ning Liang <nliang@book.com>
2009-10-29 22:07:31 -0400

    invalid paths

--- borrowed_item/lending_borrowed.rb
+++ borrowed_item/lending_borrowed.rb
@@ -11,13 +11,29 @@ def generate_valid_path(path)
   puts "}"
 end
 
+def generate_invalid_path(path)
+  puts ""
+  names = path.collect do |transition| transition["name"] end
+  puts "public void test#{names.to_s}() throws Exception {"
+  0..(path.length - 2).each do |index|
+    transition = path[index]
+    puts "\t#{transition["name"].downcase}Product(null);" unless transition["name"].eql? "Start"
+    puts "\tverify(\"#{transition["to"]}\");"
+  end
+  puts "\t#{path.last["name"].downcase}Product(Exception.class);" unless transition["name"].eql? "Start"
+  puts "\tverify(\"#{transition["to"]}\");"
+  puts "}"
+end
+
 explorer = GraphPathExplorer.new("lending_borrowed.json")
 explorer.valid_paths.each do |path| 
   # We cannot programmatically call expire
-  generate_valid_path(path) unless path.find do |transition|
-    transition["name"].eql? "Expire"
-  end
+  generate_valid_path(path) unless path.find do |transition| transition["name"].eql? "Expire" end
 end
-#explorer.invalid_paths.each do |path| generate_path(path) end
+
+explorer.invalid_paths.each do |path| 
+  # We cannot programmatically call expire
+  generate_invalid_path(path) unless path.find do |transition| transition["name"].eql? "Expire" end
+end
 
 puts "#{explorer.valid_paths.length} valid paths and #{explorer.invalid_paths.length} invalid paths"
